9104aba38614e331f7ad685b075d560715c30b9c
Steven Ruppert <blendmaster@gmail.com>
2013-03-03 21:10:23 -0700

    Add go app skeleton
    I sure hope that using go won't bite me later...

---
+++ highlander.go
@@ -0,0 +1,30 @@
+/* Highlander: prominent topology for prominent people
+ * Author: Steven Ruppert
+ * For CSCI 447 Scientific Visualization, Spring 2013
+ * Colorado School Of Mines
+ */
+package main
+
+import (
+	"fmt"
+	//"image"
+	"image/png"
+	//"io"
+	"os"
+)
+
+func main() {
+	img, err := png.Decode(os.Stdin)
+	if err != nil {
+		fmt.Println("Can't read that!", err)
+		return
+	}
+
+	b := img.Bounds()
+
+	for y := b.Min.Y; y < b.Max.Y; y++ {
+		for x:= b.Min.X; x < b.Max.X; x++ {
+			fmt.Println(img.At(x, y))
+		}
+	}
+}
