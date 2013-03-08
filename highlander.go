9223ab486d3ba33491882da7e8618856505f4758
Steven Ruppert <blendmaster@gmail.com>
2013-03-08 00:06:16 -0700

    Handle threshold command line parameter

--- highlander.go
+++ highlander.go
@@ -11,18 +11,24 @@ import (
 	//"image"
 	"image/png"
 	//"io"
+	"flag"
 	"highlander/prominence"
 	"os"
 )
 
 func main() {
+
+	var threshold uint
+	flag.UintVar(&threshold, "threshold", 8000, "threshold of prominence for output features")
+	flag.Parse()
+
 	img, err := png.Decode(os.Stdin)
 	if err != nil {
 		log.Fatalln("Couldn't read png image from Stdin!", err)
 	}
 
 	// TODO pass threshold and image as command line args
-	features := prominence.ProminentFeatures(img, 8000)
+	features := prominence.ProminentFeatures(img, uint16(threshold))
 	for e := features.Front(); e != nil; e = e.Next() {
 		fmt.Println(e.Value)
 	}
